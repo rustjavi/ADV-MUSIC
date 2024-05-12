1e52e36495502f5aaa15bd004b2c3378674db31d
rustjavi <rustjavi@proton.me>
2024-05-12 07:54:44 -0400

    Update history.js

--- musicCommands/history.js
+++ musicCommands/history.js
@@ -15,8 +15,7 @@ module.exports = {
       .setColor('#2b71ec')
      .setAuthor({
           name: 'History!',
-          iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488720242229450/clock.png?ex=656b6a2e&is=6558f52e&hm=6a03f4358573633ecf29cee2bb85eb5da4cc2ec94e6e79595b499009993a51b2&',
-          url: 'https://kick.com/rustjavi'
+          iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488720242229450/clock.png?ex=656b6a2e&is=6558f52e&hm=6a03f4358573633ecf29cee2bb85eb5da4cc2ec94e6e79595b499009993a51b2&'
         })
       .setDescription(history.map((song, index) => `${index + 1}. ${song.title} - \n[${song.link}]`).join('\n'));
 
