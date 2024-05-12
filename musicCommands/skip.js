81267740404c5a8bae0e23ced4b115e4b78d11a2
rustjavi <rustjavi@proton.me>
2024-05-12 07:54:16 -0400

    Update skip.js

--- musicCommands/skip.js
+++ musicCommands/skip.js
@@ -30,7 +30,7 @@ module.exports = {
            .setColor('#2b71ec')
      .setAuthor({
           name: 'Skipped Song!',
-          iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721253052426/right-chevron-.png?ex=656b6a2e&is=6558f52e&hm=7a73aa51cb35f25eba52055c7b4a1b56bbf3a6d150643adc15b52dc533236956&',
+          iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721253052426/right-chevron-.png?ex=656b6a2e&is=6558f52e&hm=7a73aa51cb35f25eba52055c7b4a1b56bbf3a6d150643adc15b52dc533236956&'
         })
           .setDescription('**Let\'s move on to the next beat...**');
         return message.reply({ embeds: [embed] });
