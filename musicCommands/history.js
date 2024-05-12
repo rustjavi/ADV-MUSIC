2f1899089be8e6c3a03d1ec88640b968db91da22
rustjavi <rustjavi@proton.me>
2024-05-12 06:55:53 -0400

    Update history.js

--- musicCommands/history.js
+++ musicCommands/history.js
@@ -16,7 +16,7 @@ module.exports = {
      .setAuthor({
           name: 'History!',
           iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488720242229450/clock.png?ex=656b6a2e&is=6558f52e&hm=6a03f4358573633ecf29cee2bb85eb5da4cc2ec94e6e79595b499009993a51b2&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://kick.com/rustjavi'
         })
       .setDescription(history.map((song, index) => `${index + 1}. ${song.title} - \n[${song.link}]`).join('\n'));
 
