7ca400c4fbb1043ea0941c74ba8c2d78834c4aa0
rustjavi <rustjavi@proton.me>
2024-05-12 05:46:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Update Out V@1.5`, type: ActivityType.Watching }],
+      activities: [{ name: `kick.com/rustjavi`, type: ActivityType.Watching }],
       status: 'idle',
     });
   }, 2000); 
